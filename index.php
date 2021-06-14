55fad2aee752cc41b5975a2d31c00e1d07e8592d
Cecile-29 <cecile.auger.pro@gmail.com>
2021-06-14 16:33:16 +0200

    push exercices PHP

--- index.php
+++ index.php
@@ -14,14 +14,14 @@
             <h2 class="col-12 text-info text-center">Créez une boucle qui affiche les nombres de 0 à 10.</h2>
         </div>
         <div class="row border border-primary rounded p-3 m-4 text-secondary justify-content-center">
-            <?php
-                $i = 0;
-                echo "les chiffres de 0 à 10 sont : <h6 class='m-2 text-center'>";
-                while ($i < 10) {
-                    // echo $i++;
-                    echo '<h6 class="m-2"> '. $i;
-                    $i++;
-                }
+              <?php 
+                // $i = 0;
+                // echo "les chiffres de 0 à 10 sont : <h6 class='m-2 text-center'>";
+                // while ($i <= 10) {
+                //     // echo $i++;
+                //     echo '<h6 class="m-2"> '. $i;
+                //     $i++;
+                // }
             //   for ($i = 0; $i < 10; $i++) {
             //      if ($i == 9) {
             //        break;
@@ -29,6 +29,20 @@
             //      echo "les chiffres de 0 à 10 sont : $i <p>";
             //    }
             ?>
+            <!-- Correction JP -->
+            <?php
+                $i = 0;
+                echo "les chiffres de 0 à 10 sont : <h6 class='m-2 text-center'>";
+                while ($i <= 10) {
+                // ici pour structurer le html on ferme la balise php pour intégrer un élément html
+                   ?>
+                    <!-- la balise p contient un élément php avecla valeur de la variable -->
+                    <p class="m-2 border border-dark rounded"><?= $i ?></p>
+                <!-- ici on ouvre une balise PHP pour l'incrémentation ce qui nous a permis de structurer les éléments html -->
+                    <?php $i++ ;
+                }
+                // on ne peut pas disposer une balise PHP dans une autre seulement encadrer les éléments avec la balise
+                ?>
         </div>
     </div>
 </body>
